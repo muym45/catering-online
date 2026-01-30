510a01df6bb6a9b3c558d89435da847ec7298c13
Muym45 <glorymuim@gmail.com>
2026-01-30 10:25:16 +0700

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\PemesananController;
+use App\Http\Controllers\DetailPemesananController;
+use App\Http\Controllers\PengirimanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// === API-like web routes (tanpa view dulu) ===
+Route::resource('pelanggans', PelangganController::class);
+Route::resource('pemesanans', PemesananController::class);
+Route::resource('detail-pemesanans', DetailPemesananController::class);
+Route::resource('pengirimans', PengirimanController::class);
